13af4c3b27ce53482377bd7c9065139059df2c92
Nuclear0709 <agp05432@gmail.com>
2026-04-14 11:13:19 +0800

    UpTo1.1

--- UserScript/liveCenter.user.js
+++ UserScript/liveCenter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TikTok Live Chat → Socket Bridge
 // @namespace    pip-chat-bridge
-// @version      1.0
+// @version      1.1
 // @description  Listen TikTok live chat and forward to socket server
 // @author       Nuclear0709
 // @match        https://livecenter.tiktok.com/*
