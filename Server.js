6f77f068cd93697c30a667ddb3fd4d65e231ee30
Nuclear0709 <agp05432@gmail.com>
2026-04-28 00:42:01 +0800

    修正設計

--- Server.js
+++ Server.js
@@ -630,7 +630,7 @@ const server = http.createServer((req, res) => {
             .find(c => c.startsWith('authToken='))
             ?.split('=')[1];
         
-        console.log("Cookie",token)
+        console.log("Cookie",token.substring(0,token.length-5)+"00000")
 
         if (!isValidToken(token)) {
             // 直接回傳 login.html，而不是 302
