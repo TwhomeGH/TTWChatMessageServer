3829fba96eafc7bd15ad4fe6fd1c3977bf769d2b
Nuclear0709 <agp05432@gmail.com>
2026-07-07 17:25:12 +0800

    修正傳遞

--- TikTok.js
+++ TikTok.js
@@ -734,7 +734,7 @@ function sendSocketMessage(user, message, img, giftImg,isMain=true,userNum=0,use
         img,
         giftImg,
         isMain:Boolean(isMain),
-        userNum,
+        userNum:Int(userNum),
         userList
     };
     
